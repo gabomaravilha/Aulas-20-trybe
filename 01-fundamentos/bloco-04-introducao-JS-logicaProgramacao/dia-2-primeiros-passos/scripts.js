8e6c38891d27a3b8a75fb21ee8d89a3c6e4a3cde
gabomaravilha <gabrieldru@gmail.com>
2022-02-04 13:35:03 -0400

    exercicio variaveis

---
+++ 01-fundamentos/bloco-04-introducao-JS-logicaProgramacao/dia-2-primeiros-passos/scripts.js
@@ -0,0 +1,8 @@
+const myName = "Gabriel"
+const birthCity = "Manaus" 
+let birthYear = 1996
+console.log (myName, birthCity, birthYear)
+birthYear = 2030
+console.log (birthYear)
+birthCity = "Curitiba"
+console.log (birthCity)
