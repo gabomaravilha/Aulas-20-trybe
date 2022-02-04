3715cffe5900b473b6007e7d88b21603ed4a1a50
gabomaravilha <gabrieldru@gmail.com>
2022-02-04 15:02:47 -0400

    exercicios if else

--- 01-fundamentos/bloco-04-introducao-JS-logicaProgramacao/dia-2-primeiros-passos/scripts.js
+++ 01-fundamentos/bloco-04-introducao-JS-logicaProgramacao/dia-2-primeiros-passos/scripts.js
@@ -7,17 +7,27 @@
 // birthCity = "Curitiba"
 // console.log (birthCity)
 
-let patientId = 50;
-let isEnrolled = true;
-const patientInfo = {
-  firstName: 'Ana',
-  lastName: 'Santos',
-};
-const patientEmail = 'ana@email.com';
+// let patientId = 50;
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
 
-const base = 5
-const height = 8
-const area = base * height
-const perimeter = (base*2) + (height*2)
-console.log (area)
-console.log (perimeter)
+// const base = 5
+// const height = 8
+// const area = base * height
+// const perimeter = (base*2) + (height*2)
+// console.log (area)
+// console.log (perimeter)
+
+const nota = 90
+
+if (nota < 60) {
+    console.log("Você foi reprovada(o)")
+} else if (nota < 80 && nota >=60) {
+    console.log("Você está na nossa lista de espera")
+} else {
+    console.log ("Parabéns, você foi aprovada(o)")
+}
