a4a5484da0a478d3c760f56193a539dbbdb23aa4
gabomaravilha <gabrieldru@gmail.com>
2022-02-04 14:43:54 -0400

    exercicio 2 concluido

--- 01-fundamentos/bloco-04-introducao-JS-logicaProgramacao/dia-2-primeiros-passos/scripts.js
+++ 01-fundamentos/bloco-04-introducao-JS-logicaProgramacao/dia-2-primeiros-passos/scripts.js
@@ -1,8 +1,23 @@
-const myName = "Gabriel"
-const birthCity = "Manaus" 
-let birthYear = 1996
-console.log (myName, birthCity, birthYear)
-birthYear = 2030
-console.log (birthYear)
-birthCity = "Curitiba"
-console.log (birthCity)
+// const myName = "Gabriel"
+// const birthCity = "Manaus" 
+// let birthYear = 1996
+// console.log (myName, birthCity, birthYear)
+// birthYear = 2030
+// console.log (birthYear)
+// birthCity = "Curitiba"
+// console.log (birthCity)
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+const base = 5
+const height = 8
+const area = base * height
+const perimeter = (base*2) + (height*2)
+console.log (area)
+console.log (perimeter)
