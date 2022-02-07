4f6cbf59b4db6692b25d98089887da111f31cdd5
gabomaravilha <gabrieldru@gmail.com>
2022-02-07 19:55:07 -0400

    pratica ate quesito 5

---
+++ 01-fundamentos/bloco-04-introducao-JS-logicaProgramacao/dia-3-array-e-loop-for/pratica/script.js
@@ -0,0 +1,20 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0
+let mediaAritmetica = 0
+let maiorNumero = numbers[0]
+    for (let index = 0; index < numbers.length; index += 1){
+    // console.log(numbers[index])
+    // soma += numbers[index]
+    // mediaAritmetica = soma/numbers.length
+    // if (numbers[index] < maiorNumero) {
+    // } else {maiorNumero = numbers[index]}
+    // if (numbers[index] > maiorNumero) {
+    //     maiorNumero = numbers[index]
+    // } else {}
+}
+// console.log(soma)
+// console.log(mediaAritmetica)
+// if (mediaAritmetica > 20) {
+//     console.log('valor maior que 20')
+// } else {console.log('valor menor ou igual a 20')}  
+// console.log(maiorNumero)
