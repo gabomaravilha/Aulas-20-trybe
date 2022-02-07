0694dca5d345e5c5a9e4fbc287e70314aefcf0f7
gabomaravilha <gabrieldru@gmail.com>
2022-02-07 15:11:34 -0400

    Exercicios arrays

---
+++ 01-fundamentos/bloco-04-introducao-JS-logicaProgramacao/dia-3-array-e-loop-for/exercicio-1/scripts.js
@@ -0,0 +1,16 @@
+// Obtenha o valor "Serviços" do array menu :
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1]
+
+console.log(menuServices);
+
+// Procure o índice do valor "Portfólio" do array menu :
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push ('Contato')
+
+console.log(menu);
