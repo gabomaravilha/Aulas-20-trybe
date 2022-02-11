2ffd5a25a442190775896fb9e6d32ef5e56f745e
gabomaravilha <gabrieldru@gmail.com>
2022-02-10 23:45:15 -0400

    pratica de objetos e funçoes finalizadas

--- 01-fundamentos/bloco-04-introducao-JS-logicaProgramacao/dia-4-logica-de-programacao/scripts.js
+++ 01-fundamentos/bloco-04-introducao-JS-logicaProgramacao/dia-4-logica-de-programacao/scripts.js
@@ -12,27 +12,35 @@
 // }console.log(palavraInvertida)
 
 // Considere o array de strings abaixo:
-// let array = ['java', 'javascript', 'python', 'html', 'css'];
-// let maiorNumero = []
-// let menorNumero = []
+let array = ['css', 'java', 'javascript', 'python', 'html','CARA', 'VAI', 'SE', 'fUDER', 'VASCO', 'JAPHE'];
+let maiorNumero = []
+let menorNumero = array[0]
 // Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
-// for (index = 0; index < array.length;index += 1) {
-//     if (maiorNumero.length < array.length) {
-//         maiorNumero = array[index]
-//     } else {menorNumero = array[index]}
-// }console.log(maiorNumero)
-// console.log(menorNumero)
+for (index = 0; index < array.length;index += 1) {
+    if (maiorNumero.length < array[index].length) {
+        maiorNumero = array[index]
+    } 
+}
+console.log(maiorNumero)
+
+for (index = 0; index < array.length;index += 1) {
+    if (menorNumero.length > array[index].length) {
+        menorNumero = array[index]
+    } else {}
+}
+console.log(menorNumero)
 
 //Um número primo é aquele divisível apenas por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que retorne o maior número primo entre 0 e 50.
-let maiorNumeroPrimo = []
-let divisores = 0 
-for (let index = 50; index >=1; index -=1) {
-    for (let index2 = index; index2 >=1; index2-=1){
-        if (index%index2 === 0) {
-            divisores += 1
-        }
-    }
-    if (divisores === 2) {
-        maiorNumeroPrimo.push(index)
-    } else {divisores = 0}
-} console.log(maiorNumeroPrimo[0])
+// let maiorNumeroPrimo = []
+// let divisores = 0 
+// for (let index = 1000; index >1; index -=1) {
+//     for (let index2 = index; index2 >=1; index2-=1){
+//         if (index%index2 === 0) {
+//             divisores += 1
+//         }
+//     } 
+//     if (divisores === 2) {
+//         maiorNumeroPrimo.push(index)
+//     } 
+//     divisores = 0
+// } console.log(maiorNumeroPrimo[0])
--- 01-fundamentos/bloco-04-introducao-JS-logicaProgramacao/dia-5-objetos-e-funcoes/pratica/script.js
+++ 01-fundamentos/bloco-04-introducao-JS-logicaProgramacao/dia-5-objetos-e-funcoes/pratica/script.js
@@ -37,16 +37,16 @@
 
 // Usando o objeto abaixo, faça os exercícios a seguir:
 // let leitor = {
-    // nome: 'Julia',
-    // sobrenome: 'Pessoa',
-    // idade: 21,
-    // livrosFavoritos: [
-    //   {
-    //     titulo: 'O Pior Dia de Todos',
-    //     autor: 'Daniela Kopsch',
-    // //     editora: 'Tordesilhas',
-    //   },
-    // ],
+//     nome: 'Julia',
+//     sobrenome: 'Pessoa',
+//     idade: 21,
+//     livrosFavoritos: [
+//       {
+//         titulo: 'O Pior Dia de Todos',
+//         autor: 'Daniela Kopsch',
+//     //     editora: 'Tordesilhas',
+//       },
+//     ],
   // };
 // Acesse as chaves nome , sobrenome e titulo , que está dentro da chave livrosFavoritos , e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
 // console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0].titulo)
@@ -60,13 +60,112 @@
 // console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos')
 
 // Parte II - Funções
-let array = 'desenvolvimento'
-let palavraInvertida = []
-function palindromo(){
-  for(let index = 0; index < array.length; index+=1){
-    palavraInvertida.unshift(array[index])
-  } return palavraInvertida
-} if (palavraInvertida === array) {
-  console.log('true')
-} else {console.log('false')}
+// Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
+// let array = 'arara'
+// let palavraInvertida = []
+// let verificaPalavra = 0
 
+// function verificaPalindrome(a) {
+//   for(let index = 0; index < array.length; index+=1){
+//     palavraInvertida.unshift(array[index])
+//   } 
+//   for(let indice = 0; indice <array.length; indice+=1){
+//     if (palavraInvertida[indice] === array[indice]) {
+//     verificaPalavra += 1
+//     } 
+//   } 
+//   if (verificaPalavra===array.length){
+//     return true 
+//   }else {return false}
+// } 
+// console.log(verificaPalindrome(array))
+// function maiorNumero(numeros) {
+  
+// }
+// Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+// let array = [2, 3, 6, 7, 10, 1]
+ 
+// function indexMaiorNumero(params) {
+//   let maiorNumero = params[0]
+//   for (index = 0; index < params.length;index += 1) {
+//     if (maiorNumero < params[index]) {
+//         maiorNumero = params[index]
+//       } 
+//     }params = params.indexOf(maiorNumero)
+//     return params
+// }
+// console.log(indexMaiorNumero(array))
+
+// Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+// let array = [2, 4, 6, 7, 10, 0, -3]
+// function indexMenorNumero(params) {
+//   let menorNumero = params[0]
+//   for (index = 0; index < params.length;index += 1) {
+//     if (menorNumero > params[index]) {
+//         menorNumero = params[index]
+//       } 
+//     }params = params.indexOf(menorNumero)
+//     return params
+// }
+// console.log(indexMenorNumero(array))
+// Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+// let array = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+// function maiorPalavra(params) {
+//   let maiorPalavra = params[0]
+//   for (index = 0; index < params.length;index += 1) {
+//     if (maiorPalavra.length < params[index].length) {
+//        maiorPalavra = params[index]
+//     }
+//     }
+//   return maiorPalavra 
+// } console.log(maiorPalavra(array))
+// Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+  // let array = [5, 2, 2, 8, 2, 3, 3, 2, 3, 3]
+  // let arrayTeste = [4, 1, 5, 5, 8, 4, 5, 2, 1, 3, 1, 7, 8, 9, 4, 2, 1, 2, 4, 1, 5]
+
+  // function maisRepetido(params) {
+  //   let comparador = 0
+  //   let numeroRepetido = 0
+  //   let numeroRepetido2 = 0
+  //   for (let index = 0; index < params.length; index+=1) {
+  //     for (let index1 = index; index1 < params.length; index1 +=1){
+  //       if (params[index1] === params[index]) {
+  //         numeroRepetido +=1
+  //       }
+  //       if (numeroRepetido >>> numeroRepetido2) {
+  //         comparador = params[index]
+  //         numeroRepetido2 = numeroRepetido
+  //       }
+  //     }
+  //     numeroRepetido = 0
+  //   }return comparador
+  // }console.log(maisRepetido(arrayTeste))
+
+  // Crie uma função que receba um número natural (número inteiro não negativo) N e retorne o somatório de todos os números de 1 até N.
+//     let numero = 1.5
+//     function somatorio(params) {
+//        let soma = 0
+//       if (numero%1 === 0 && numero >=0){
+//         for (let index = 0; index <= params; index+=1) {
+//         soma = index + soma
+//         }
+//       } else soma = 'Não é um numero inteiro não negativo'
+//       return soma
+//     }console.log(somatorio(numero))
+// console.log((3/2)%1)
+// Crie uma função que receba uma string word e outra string ending . Verifique se a string ending é o final da string word .
+//  Considere que a string ending sempre será menor que a string word .
+let palavra = 'joaofernando'
+let palavra1 = 'fernan'
+  function verificaFim(params, params2) {
+    let letrasIguais= 0
+    let resposta = 0
+    for (let index = 0; index < params2.length; index+=1) {
+      if (params2[(params2.length-1) - index] === params[(params.length-1)-index]){
+        letrasIguais+=1
+      }
+      if(params2.length === letrasIguais) {
+        resposta = true
+      }else {resposta = false}
+    }return resposta
+  }console.log(verificaFim(palavra, palavra1))
