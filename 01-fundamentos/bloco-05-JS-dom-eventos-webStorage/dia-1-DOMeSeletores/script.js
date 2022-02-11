a8135405a795edfd11c80108409302cb860fabf2
gabomaravilha <gabrieldru@gmail.com>
2022-02-11 18:56:16 -0400

    atividades conteudo concluidas

---
+++ 01-fundamentos/bloco-05-JS-dom-eventos-webStorage/dia-1-DOMeSeletores/script.js
@@ -0,0 +1,15 @@
+// let titulo = document.querySelector('#header-container')    
+// titulo.style.background = 'green'
+// let urgente = document.querySelector('.emergency-tasks')
+// urgente.style.background = 'pink'
+// let noUrgente = document.querySelector('.no-emergency-tasks')
+// noUrgente.style.background = 'yellow'
+// let tituloUrgente = document.querySelectorAll('.titles-emergency')
+// let tituloNaoUrgente = document.querySelectorAll('.titles-no-emergency')
+// for (let index = 0; index < tituloUrgente.length; index+=1) {
+//     tituloUrgente[index].style.background = 'purple';
+//     tituloNaoUrgente[index].style.background = 'black'} 
+// let rodape = document.getElementsByTagName('footer')
+// rodape[0].style.backgroundColor = 'green'
+
+
