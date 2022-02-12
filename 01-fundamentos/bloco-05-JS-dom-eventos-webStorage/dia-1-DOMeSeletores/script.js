2361c2a17d604933a54c6db2f368821dc897af57
gabomaravilha <gabrieldru@gmail.com>
2022-02-11 21:34:11 -0400

    exercicios dom e seletores concluido

--- 01-fundamentos/bloco-05-JS-dom-eventos-webStorage/dia-1-DOMeSeletores/script.js
+++ 01-fundamentos/bloco-05-JS-dom-eventos-webStorage/dia-1-DOMeSeletores/script.js
@@ -1,15 +1,29 @@
-// let titulo = document.querySelector('#header-container')    
-// titulo.style.background = 'green'
-// let urgente = document.querySelector('.emergency-tasks')
-// urgente.style.background = 'pink'
-// let noUrgente = document.querySelector('.no-emergency-tasks')
-// noUrgente.style.background = 'yellow'
-// let tituloUrgente = document.querySelectorAll('.titles-emergency')
-// let tituloNaoUrgente = document.querySelectorAll('.titles-no-emergency')
-// for (let index = 0; index < tituloUrgente.length; index+=1) {
-//     tituloUrgente[index].style.background = 'purple';
-//     tituloNaoUrgente[index].style.background = 'black'} 
-// let rodape = document.getElementsByTagName('footer')
-// rodape[0].style.backgroundColor = 'green'
+let paragrafo = 'Ainda assim, existem dúvidas a respeito de como o julgamento imparcial das eventualidades estimula a padronização do impacto na agilidade decisória.'
+function mudarP(arrayTexto) {
+    document.getElementsByTagName('p')[0].innerText = arrayTexto
+}   mudarP(paragrafo)
+let fundo = 'rgb(76,164,109)'
+function mudarFundo (cor) {
+document.getElementsByClassName('main-content')[0].style.background = cor
+}   mudarFundo(fundo)
+function mudarFundoCentro (cor1) {
+    document.getElementsByClassName('center-content')[0].style.background = cor1
+}   mudarFundoCentro('white')
+function MudarH1 (texto) {
+    document.getElementsByTagName ('h1')[0].innerText = texto    
+}   MudarH1('Exercício 5.1 - Javascript')
+function mudarMaiusculoP () {
+    let tamanhoP = document.getElementsByTagName('p')
+    for (let index = 0; index < tamanhoP.length; index+=1) {
+    tamanhoP[index].innerText = tamanhoP[index].innerText.toUpperCase()
+    } 
+} mudarMaiusculoP()
 
-
+function mostrarP () {
+    let conteudoP = document.getElementsByTagName('p')
+    let aux = conteudoP
+    for (let index1 = 0; index1 < aux.length; index1+=1) {
+        conteudoP = document.getElementsByTagName('p')[index1].innerText
+        console.log(conteudoP)
+    }
+} mostrarP()
