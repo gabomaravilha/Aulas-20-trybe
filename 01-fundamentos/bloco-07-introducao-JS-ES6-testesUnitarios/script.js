ef00a47c093c6db2b59e459fee8bdb4b56b9519e
gabomaravilha <gabrieldru@gmail.com>
2022-03-08 13:58:11 -0400

    começo exercicios 7-2

--- 01-fundamentos/bloco-07-introducao-JS-ES6-testesUnitarios/script.js
+++ 01-fundamentos/bloco-07-introducao-JS-ES6-testesUnitarios/script.js
@@ -46,14 +46,77 @@
 // const maiorPalavra = frase => frase.split(' ').sort((palavra1, palavra2) => palavra2.length - palavra1.length)[0] 
 // console.log(maiorPalavra('Antônio foi no banheiro e não sabemos o que aconeu'))
 
-const botaoContador = () =>{
-  const botao = document.querySelector('button')
-  const criarP = document.createElement('p')
-  let numeroCliques = 0
-  const body = document.querySelector('body')
-  botao.addEventListener('click', () => {
-    criarP.innerText = numeroCliques += 1
-    body.appendChild(criarP)
-  })
-} 
-botaoContador()
+// const botaoContador = () =>{
+//   const botao = document.querySelector('button')
+//   const criarP = document.createElement('p')
+//   let numeroCliques = 0
+//   const body = document.querySelector('body')
+//   botao.addEventListener('click', () => {
+//     criarP.innerText = numeroCliques += 1
+//     body.appendChild(criarP)
+//   })
+// } 
+// botaoContador()
+
+// const customer1 = {
+//   firstName: 'Roberto',
+//   age: 22,
+//   job: 'Teacher',
+// };
+
+// console.log(customer1);
+
+// customer1.lastName = 'Faria';
+// console.log(customer1);
+
+// const customer2 = {
+//   firstName: 'Maria',
+//   age: 23,
+//   job: 'Medic',
+// };
+
+// console.log(customer2);
+// customer2['lastName'] = 'Silva';
+// console.log(customer2);
+
+// const customer = {
+//   firstName: 'Roberto',
+//   age: 22,
+//   job: 'Teacher',
+// };
+
+// let newKey = 'lastName';
+// const lastName = 'Ferreira';
+// customer[newKey] = lastName;
+// newKey = 'fullName';
+// const fullName = `${customer.firstName} ${customer.lastName}`;
+// customer[newKey] = fullName;
+// console.log(customer);
+
+// const student1 = {
+//   Html: 'Muito Bom',
+//   Css: 'Bom',
+//   JavaScript: 'Ótimo',
+//   SoftSkills: 'Ótimo',
+// };
+
+// const student2 = {
+//   Html: 'Bom',
+//   Css: 'Ótimo',
+//   JavaScript: 'Ruim',
+//   SoftSkills: 'Ótimo',
+//   Git: 'Bom', // chave adicionada
+// };
+
+// const listSkills = (student) => {
+//   const arrayOfSkills = Object.keys(student);
+//   for (const index in arrayOfSkills) {
+//     console.log(`${arrayOfSkills[index]}, Nível: ${student[arrayOfSkills[index]]}`);
+//   }
+// };
+
+// console.log('Estudante 1');
+// listSkills(student1);
+
+// console.log('Estudante 2');
+// listSkills(student2);
